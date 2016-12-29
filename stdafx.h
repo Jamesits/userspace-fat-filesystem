a94be7962ae2317726685a8d89fc3ef4fd535b5a
James Swineson <i@swineson.me>
2016-12-29 12:53:52 +0800

    Add debug macro

--- stdafx.h
+++ stdafx.h
@@ -10,6 +10,22 @@
 #include <wordexp.h>
 #include <sys/wait.h>
 
+#ifndef NDEBUG
+#  include <stdio.h>
+#  include <errno.h>
+#  define DEBUG(format, ...)					\
+({								\
+	int errno_save = errno;					\
+	fprintf(stderr, "%s:%u %s(): ",				\
+		  __FILE__, __LINE__, __func__);		\
+	fprintf(stderr, format, ##__VA_ARGS__);			\
+	putc('\n', stderr);					\
+	errno = errno_save;					\
+})
+#else
+#  define DEBUG(format, ...)
+#endif
+
 #include "config.h"
 #include "util.h"
 #include "main.h"
